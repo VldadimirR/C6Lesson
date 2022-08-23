a7737fcaca93f252006eab31475f6e0267b656a3
VladimirR <Vova.ru0007@yandex.ru>
2022-08-23 17:14:06 +0500

    zadanie41

---
+++ Zad41/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3 */
+
+Console.Clear();
+Console.WriteLine("Введите 5  чисел через пробел: ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse); // считываем строку с консоли и получаем из этой строки массив
+
+int count = 0;
+void NumbersGreaterThanZero(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+        if (arr[i] > 0)
+        {
+            count++;
+        }
+    Console.Write($"Количество цифр больше нуля: {count}");
+}
+NumbersGreaterThanZero(arr);
